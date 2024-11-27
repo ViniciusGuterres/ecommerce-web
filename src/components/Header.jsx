b94b4ed1fa725b057fa9c80cfae414d01b152219
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-11-26 22:43:29 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,13 +75,6 @@ function Header() {
                         :
                         null
                 }
-
-                <a
-                    href='/customerCrud'
-                    className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                >
-                    Editar Dados
-                </a>
             </div>
         );
     }
